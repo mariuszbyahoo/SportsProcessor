5faa11acbfe46ad361182ec803b5bd44cd48d19f
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2024-10-03 15:55:00 +0200

    implementing InternalProcessor

--- SportsProcessor/InternalProcessor.cs
+++ SportsProcessor/InternalProcessor.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Diagnostics;
 using SportsProcessor.Contracts;
 using SportsProcessor.Models.Input;
 using SportsProcessor.Models.Output;
@@ -11,6 +9,32 @@ public class InternalProcessor : IInternalProcessor
 
     public ProcessedActivity Process(ActivitySummary activitySummary, List<LapData> lapsData, List<SampleData> sampleDatas)
     {
-        throw new NotImplementedException();
+        var result = new ProcessedActivity
+        {
+            UserId = activitySummary.UserId,
+            ActivityId = activitySummary.ActivityId,
+            ActivityType = activitySummary.ActivityType,
+            MaxHeartRate = activitySummary.MaxHeartRateInBeatsPerMinute,
+            Laps = new List<ProcessedLap>()
+        };
+
+        foreach (var lap in lapsData)
+        {
+            var dataSamples = sampleDatas.Where(s => s.SampleType == 2).ToList();
+
+            result.Laps.Add(new ProcessedLap
+            {
+                Distance = lap.TotalDistanceInMeters,
+                Duration = lap.TimerDurationInSeconds,
+                StartTime = lap.StartTimeInSeconds,
+                HeartRateSamples = dataSamples.Select((sample, index) => new HeartRateSample
+                {
+                    SampleIndex = index,
+                    HeartRate = sample.Data.FirstOrDefault()
+                }).ToList()
+            });
+        }
+
+        return result;
     }
 }
