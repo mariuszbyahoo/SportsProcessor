9676ce5cc4f95f75e98b23200f8d919ae98b6417
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2024-10-03 16:49:13 +0200

    fixing code in InternalProcessor

--- SportsProcessor/InternalProcessor.cs
+++ SportsProcessor/InternalProcessor.cs
@@ -7,7 +7,7 @@ namespace SportsProcessor;
 public class InternalProcessor : IInternalProcessor
 {
 
-    public ProcessedActivity Process(ActivitySummary activitySummary, List<LapData> lapsData, List<SampleData> sampleDatas)
+    public ProcessedActivity Process(ActivitySummary activitySummary, List<LapData> laps, List<SampleData> sampleDatas)
     {
         var result = new ProcessedActivity
         {
@@ -17,21 +17,37 @@ public class InternalProcessor : IInternalProcessor
             MaxHeartRate = activitySummary.MaxHeartRateInBeatsPerMinute,
             Laps = new List<ProcessedLap>()
         };
-
-        foreach (var lap in lapsData)
+        var dataSamples = sampleDatas.Where(s => s.SampleType == 2).ToList();
+        /*
+            We're only interested in heart rate samples, type == 2
+            Due to the lack of any timezone data within HeartRateSample, 
+            to make it clear on which sample to assign to which lap, I 
+            will use FIFO then. (first lap in the set has two first samples from sample data set)
+        */
+        foreach (var lap in laps)
         {
-            var dataSamples = sampleDatas.Where(s => s.SampleType == 2).ToList();
-
+            int index = 0;
+            var samples = new SampleData[] 
+            { 
+                dataSamples.FirstOrDefault(),
+                dataSamples.FirstOrDefault() 
+            };
+            var samplesList = new List<HeartRateSample>();
+            foreach (var sample in samples) 
+            {
+                foreach (var sampleData in sample.Data)
+                {
+                    samplesList.Add(new HeartRateSample(){ HeartRate = sampleData, SampleIndex = index });
+                    index ++;
+                }
+            }
+            // dwa DataSample na jedno okrązenie 
             result.Laps.Add(new ProcessedLap
             {
                 Distance = lap.TotalDistanceInMeters,
                 Duration = lap.TimerDurationInSeconds,
                 StartTime = lap.StartTimeInSeconds,
-                HeartRateSamples = dataSamples.Select((sample, index) => new HeartRateSample
-                {
-                    SampleIndex = index,
-                    HeartRate = sample.Data.FirstOrDefault()
-                }).ToList()
+                HeartRateSamples = samplesList
             });
         }
 
