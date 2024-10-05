3a7ffafccccd8c605a98f051c879e6ebd4f989a1
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2024-10-05 12:20:52 +0200

    adding comment

--- SportsProcessor/DataLoader.cs
+++ SportsProcessor/DataLoader.cs
@@ -3,6 +3,11 @@ using SportsProcessor.Models.Input;
 
 namespace SportsProcessor;
 
+/// <summary>
+/// Class contains methods being a wrapper around JsonSerializer.Deserialize<T>(string).
+/// We assume, that System.Text.Json assembly is reliable enough to omit adding any 
+/// tests of such methods
+/// </summary>
 internal class DataLoader
 {
     public List<LapData> LoadLapData(string json)
